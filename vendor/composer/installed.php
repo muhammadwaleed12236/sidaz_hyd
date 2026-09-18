2c2955026a9edafbecc674ff4893288854300ab3
muhammadwaleed12236 <muhammadwaleed647@gmail.com>
2026-09-18 18:46:25 +0500

    done

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9764e7197b5bad1899600ef213889aa9a17e421f',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -434,9 +434,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9764e7197b5bad1899600ef213889aa9a17e421f',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
